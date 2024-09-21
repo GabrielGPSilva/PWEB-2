38b2afa0b50a581dcc14513696041e05f4e40069
GabrielGPSilva <gabrielpereirahp@gmail.com>
2024-09-20 21:24:07 -0300

    Add files via upload
    Questão 05 - PWEB2

---
+++ q05.php
@@ -0,0 +1,17 @@
+<?php
+	$valor = $_GET['valor'];
+	
+	if ($valor % 4 == 0) {
+		if ($valor % 100 != 0) {
+			print 'Não é bissexto!';
+		} else {
+			if ($valor % 400 == 0) {
+				print 'É bissexto!';
+			} else {
+				print 'Não é bissexto!';
+			}
+		}
+	} else {
+		print 'Não é bissexto!';
+	}
+?>
